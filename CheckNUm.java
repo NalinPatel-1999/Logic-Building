962b2e789c629166250ddda16f43b34fc2fa748e
Nalin <nalin00patel@gmail.com>
2026-03-17 13:27:21 +0530

    Initial commit 1st program check num

---
+++ CheckNUm.java
@@ -0,0 +1,16 @@
+package test;
+
+public class CheckNUm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = -1;
+		
+		if (num == 0) System.out.println("Number is Zero");
+		else if (num < 0) System.out.println("Num is -");
+		else System.out.println("Num is +");
+
+	}
+
+}
