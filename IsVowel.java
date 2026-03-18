d870c91db8999f3123213a1f2179c60c6fd945fe
Nalin <nalin00patel@gmail.com>
2026-03-18 12:21:01 +0530

    IsVowel commited

---
+++ IsVowel.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+
+public class IsVowel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scr = new Scanner (System.in);
+		
+		char ch = scr.next().charAt(0);
+		
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+			    ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+			    System.out.println(ch + " is a vowel");
+			} else {
+			    System.out.println(ch + " is a consonant");
+			}
+		
+
+	}
+
+}
