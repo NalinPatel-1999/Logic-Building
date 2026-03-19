3d2fe98147f90b96044a468df61d62e255d0a2fd
Nalin <nalin00patel@gmail.com>
2026-03-19 17:03:54 +0530

    Lvl - 2 Q4

---
+++ MultipleOfOther.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.util.Scanner;
+
+public class MultipleOfOther {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scr = new Scanner (System.in);
+		
+		int num1 = scr.nextInt();
+		int num2 = scr.nextInt();
+		
+		if(num1<num2) {
+			if (num2%num1==0) System.out.println(num1+" is multiplier of "+num2);
+			else System.out.println("Not a multiplier");
+		}
+		else {
+			if (num1%num2==0) System.out.println(num2+" is multiplier of "+num1);
+			else System.out.println("Not a multiplier");
+			
+		}
+		
+		
+
+	}
+
+}
