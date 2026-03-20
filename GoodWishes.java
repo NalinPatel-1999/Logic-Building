5234df8215894694d1e0c6cdfd2fa6ff42fe7e82
Nalin <nalin00patel@gmail.com>
2026-03-20 09:03:02 +0530

    Lvl-2 Q5

---
+++ GoodWishes.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Scanner;
+
+public class GoodWishes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scr = new Scanner (System.in);
+		
+		int time = scr.nextInt();
+		
+		if (time<0 || time >23) {
+			System.out.println("Invalid time entered - "+time);
+			return;
+		}
+		
+		if (time>= 0 && time <= 12)System.out.println("GM");
+		else if (time>12 && time <=16) System.out.println("GA");
+		else if (time>16 && time <=20) System.out.println("GE");
+		else System.out.println("GN");
+
+	}
+
+}
