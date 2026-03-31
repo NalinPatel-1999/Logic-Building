fd9fd480f0be56d6ab5761ec7a111e7f48273c8f
Nalin <nalin00patel@gmail.com>
2026-03-31 12:22:08 +0530

    hashSet

--- collestions/Hashset_Learn.java
+++ collestions/Hashset_Learn.java
@@ -1,9 +1,23 @@
 package collestions;
 
+import java.util.HashSet;
 public class Hashset_Learn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		HashSet <String> cars = new HashSet<>();
+		
+		cars.add("Volvo");
+		cars.add("BMW");
+		cars.add("Ford");
+		cars.add("BMW");
+		cars.add("Mazda");
+		cars.add(null);
+		cars.add(null);
+		
+		System.out.println(cars);
+		
 
 	}
 
