8911febef76e404e65ca992681177437a0271b04
Nalin <nalin00patel@gmail.com>
2026-03-31 12:17:10 +0530

    collections Lists, sets, maps

---
+++ collestions/Hashset_Learn.java
@@ -0,0 +1,10 @@
+package collestions;
+
+public class Hashset_Learn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ collestions/Learn_ArrayList.java
@@ -0,0 +1,21 @@
+package collestions;
+import java.util.ArrayList;
+public class Learn_ArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList <String> al = new ArrayList<>();
+		
+		al.add("Nalin");
+		al.add("Patel");
+		al.add("Sachin");
+		al.add("Bamotriya");
+		al.add("Mohit");
+		al.add("Singh");
+		
+		System.out.println(al);
+
+	}
+
+}
