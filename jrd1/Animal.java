551a1c695c1c9075969e207c53b9570d335b28ec
Nalin <nalin00patel@gmail.com>
2026-03-31 08:59:47 +0530

    Class Animal

---
+++ jrd1/Animal.java
@@ -0,0 +1,19 @@
+package jrd1;
+
+class Animal {
+	
+	void sound() {
+		System.out.println("Animals make sound");
+	}
+
+}
+
+class dog extends Animal{
+	
+	void sound() {
+		System.out.println("Barks");
+	}
+}
+
+
+
---
+++ jrd1/Test_Animal.java
@@ -0,0 +1,22 @@
+package jrd1;
+
+public class Test_Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Animal a = new Animal();
+		
+		a.sound();
+		
+		dog d = new dog ();
+		
+		d.sound();
+		
+		Animal an = new dog();
+		
+		an.sound();
+
+	}
+
+}
